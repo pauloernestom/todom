af039f608709a69b29cb9d770c894e768df9ec9e
Sérgio Moura <smouracalmon@gmail.com>
2020-05-27 15:31:11 -0300

    Um bodado de coisa

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ require("dotenv").config();
 const path = require("path");
 const express = require('express');
 
+// Trazendo routers
+const AuthRouter = require("./routes/AuthRouter");
+
 // Criando o app express
 const app = express();
 
+app.use(express.json());
+
 // Configurando pasta public para requisições estáticas
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api/', AuthRouter);
 
 // Levantando o servidor
 app.listen(process.env.HTTP_PORT);
